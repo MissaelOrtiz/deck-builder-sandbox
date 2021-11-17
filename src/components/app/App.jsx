927f18e00bbd4cf35f9038b0236963f7cd495980
AngelDOrtiz <aortizghs@gmail.com>
2021-11-16 16:42:53 -0800

    battlefield layout worked on.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Battle from '../display-components/Battle';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Battle />;
 }
--- src/components/display-components/Battle.jsx
+++ src/components/display-components/Battle.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import styles from './Battle.css'
 
 export default class Battle extends Component {
     render() {
         return (
-            <div>
+            <div className={styles['battlefield']}>
 
                 <section className={styles['combat-top']}>
                     <div className={styles['combat-health-bar']}>
@@ -11,8 +12,11 @@ export default class Battle extends Component {
                     </div>
                 </section>
                 <section className={styles['combat-middle']}>
-                    <div className={styles['combat-player-sprite']}></div>
-                    <div className={styles['combat-enemy-sprite']}></div>
+                    <div className={styles['player-relics']}></div>
+                    <div className={styles['combat-middle-sprites']}>
+                        <div className={styles['player-sprite']}></div>
+                        <div className={styles['enemy-sprite']}></div>
+                    </div>
                 </section>
                 <section className={styles['combat-bot']}>
                     <div className={styles['combat-bot-left']}>
